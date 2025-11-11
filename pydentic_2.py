0657535009a949d2110bd501248042fedc098231
L <lamir68493@tutamail.com>
2025-11-11 22:11:19 +0200

    pydentic2

--- pydentic_2.py
+++ pydentic_2.py
@@ -1,1 +1,12 @@
-print(111)
+from typing import List
+from pydantic import BaseModel
+
+class Person(BaseModel):
+    name: str
+    age: int
+
+person = Person(name="John", age=20)
+js = person.model_dump_json(indent=4)
+print(js)
+with open("person.json", "w") as f:
+    f.write(js)
